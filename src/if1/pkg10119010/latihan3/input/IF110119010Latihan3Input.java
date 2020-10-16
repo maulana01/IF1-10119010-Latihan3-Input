35b30e28380edfce5b47a93377701784b40bc442
maulana01 <maulanaimammalik4@gmail.com>
2020-10-16 10:54:04 +0700

    new commit

---
+++ src/if1/pkg10119010/latihan3/input/IF110119010Latihan3Input.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119010.latihan3.input;
+
+import java.util.Scanner;
+/**
+ *
+ * @author
+ * NAMA     : Maulana Imam Malik
+ * KELAS    : IF1
+ * NIM      : 10119010
+ * Deskripsi Program : Program ini berisi program untuk menampilkan value
+ *                     dari input user
+ */
+public class IF110119010Latihan3Input {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah "+nama);
+    }
+    
+}
